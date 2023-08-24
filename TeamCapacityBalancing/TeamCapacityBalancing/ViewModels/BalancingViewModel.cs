6239b8157c47e732bececfa45bf41bb4ee84aa71
Kiwy37 <102227689+kiwy37@users.noreply.github.com>
2023-08-24 09:01:11 +0300

    Add Avalonia Material Icon and Create BalancingPage

---
+++ TeamCapacityBalancing/TeamCapacityBalancing/ViewModels/BalancingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCapacityBalancing.ViewModels;
+
+public sealed partial class BalancingViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool _isPaneOpen = true;
+
+    [ObservableProperty]
+    private SplitViewDisplayMode _mode = SplitViewDisplayMode.CompactInline;
+}
