639d1a66186e7ec8a163ecf19c00b8ce8f4961c1
Tiberio1234 <tiberio.brinzea@gmail.com>
2023-08-24 13:47:47 +0300

    interface for data provider

--- TeamCapacityBalancing/TeamCapacityBalancing/Services/ServicesAbstractions/IDataProvider.cs
+++ TeamCapacityBalancing/TeamCapacityBalancing/Services/ServicesAbstractions/IDataProvider.cs
@@ -1,6 +1,17 @@
-﻿namespace TeamCapacityBalancing.Services.ServicesAbstractions
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeamCapacityBalancing.Models;
+
+namespace TeamCapacityBalancing.Services.ServicesAbstractions
 {
     public interface IDataProvider
     {
+        public Task<List<IssueData>> GetAllEpicAsync(string teamLeaderUsername);
+        public Task<List<IssueData>> GetAllStoriesAsync(string teamLeaderUsername);
+        public Task<List<IssueData>> GetAllUsersAsync();
+
+        public Task<List<IssueData>> GetStoriesByEpicAsync(string epicId);
+
+
     }
 }
