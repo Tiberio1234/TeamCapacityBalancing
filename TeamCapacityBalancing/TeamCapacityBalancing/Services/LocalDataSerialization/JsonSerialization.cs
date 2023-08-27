e5b191437b983a090a3686c401fc5c53d74db9c0
Tiberio1234 <tiberio.brinzea@gmail.com>
2023-08-27 19:25:48 +0300

    Serialize data

---
+++ TeamCapacityBalancing/TeamCapacityBalancing/Services/LocalDataSerialization/JsonSerialization.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamCapacityBalancing.Models;
+using TeamCapacityBalancing.Services.ServicesAbstractions;
+
+namespace TeamCapacityBalancing.Services.LocalDataSerialization
+{
+    internal class JsonSerialization : IDataSerialization
+    {
+        public List<UserStoryDataSerialization> DeserializeData(string filename)
+        {
+
+            return JsonConvert.DeserializeObject<List<UserStoryDataSerialization>>(File.ReadAllText("../../" + filename));
+         
+        }
+
+        public void SerializeData(List<UserStoryDataSerialization> userStoryDataSerializations,string filename)
+        {
+            File.WriteAllText("../../" + filename, JsonConvert.SerializeObject(userStoryDataSerializations));
+        }
+    }
+}
---
+++ TeamCapacityBalancing/TeamCapacityBalancing/Services/ServicesAbstractions/IDataSerialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamCapacityBalancing.Models;
+
+namespace TeamCapacityBalancing.Services.ServicesAbstractions
+{
+    interface IDataSerialization
+    {
+        public void SerializeData(List<UserStoryDataSerialization> userStoryDataSerializations, string filename);
+        List<UserStoryDataSerialization> DeserializeData(string filename);
+    }
+}
