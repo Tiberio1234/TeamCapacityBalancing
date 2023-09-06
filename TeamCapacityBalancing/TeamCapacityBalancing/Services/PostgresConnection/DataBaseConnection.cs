37be1003a515b4c0302a94a06b841b0b0a57b29d
Tabacaru Rares Andrei <rares.andrei.tabacaru@gmail.com>
2023-09-06 13:02:28 +0300

    Small changes

--- TeamCapacityBalancing/TeamCapacityBalancing/Services/PostgresConnection/DataBaseConnection.cs
+++ TeamCapacityBalancing/TeamCapacityBalancing/Services/PostgresConnection/DataBaseConnection.cs
@@ -8,14 +8,9 @@ namespace TeamCapacityBalancing.Services.Postgres_connection
         private static readonly object _lock = new object();
 
         private const string Host = "localhost";
-        private const string User = "postgres";
-<<<<<<< HEAD
-        private const string DBname = "Jira";
-        private const string Password = "ciocanitoarea";
-=======
-        private const string DBname = "postgres";
-        private const string Password = "123456";
->>>>>>> 017329394535519cecafbad4da09cd6c864e474d
+        private const string User = "Test";
+        private const string DBname = "Jira2";
+        private const string Password = "123";
         private const string Port = "5432";
 
         private string connectionString = String.Format(
